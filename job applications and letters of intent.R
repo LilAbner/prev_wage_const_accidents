76340bdd2e03eee72562daccd264451d428d8552
LilAbner <kaylie.abner@gmail.com>
2025-11-29 17:17:12 -0500

    Added more descriptions

---
+++ job applications and letters of intent.R
@@ -0,0 +1,11 @@
+#---------------New Building Alterations and Letters of Intent--------
+
+#Purpose:
+# 485x will be new buildings that filed with Department of Buildings most likely after 2022 
+# They may have already sent in letters of intent to Housing Preservation and Development
+# But, it's possible that they have not sent in the letter of intent
+# Most new buildings will probably use 485x, so we can also use all new buildings 
+# and be reasonably confident the majority will use 485x. 
+
+#----------------------Libraries-----------------
+
